09f2ad9eb3f235ea3b4c5d6898956a764ad9cfa1
smondal8 <54061876+smondal8@users.noreply.github.com>
2020-11-19 15:49:37 +0530

    login inprogress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationBookComponent } from './Books-Registration/book-registration.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/authGuard';
 
 
 const routes: Routes = [
-  {path: 'library', component: RegistrationBookComponent},
-  {path: 'books-details/:id', component: BookDetailsComponent}
+  {path: '', pathMatch: 'full',redirectTo: 'login'},
+  {path: 'login',component: AuthComponent},
+  {path: 'library', canActivate: [AuthGuard], component: RegistrationBookComponent},
+  {path: 'books-details/:id', canActivate: [AuthGuard],component: BookDetailsComponent}
   //{path: "*", component: <>}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
